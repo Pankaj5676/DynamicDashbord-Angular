ce7f58179524a43b6ec669dea335d6c1cf9b6e9d
Pankaj <pankajbhogade1@gmail.com>
2025-04-29 16:22:42 +0530

    adding drag and drop

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -4,25 +4,66 @@ import { ReportWidgetComponent } from '../report-widget/report-widget.component'
 import { TableWidgetComponent } from '../table-widget/table-widget.component';
 import { CommonModule } from '@angular/common';
 import{ Chart,registerables} from 'chart.js'
+import { CdkDragDrop, DragDropModule ,moveItemInArray} from '@angular/cdk/drag-drop'
+import { NewComponentComponent } from '../../new-component/new-component.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule,ReportWidgetComponent,TableWidgetComponent,ChartWidgetComponent],
+  imports: [CommonModule,DragDropModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  widgets = [
+
+  widgets: { type: string }[] = [];
+  
+
+  // widgets = [
+  //   { type: 'chart' },
+  //   { type: 'report' },
+  //   { type: 'table' },
+  //   {type:'newCompo'}
+  // ];
+
+  availableWidgets = [
     { type: 'chart' },
     { type: 'report' },
-    { type: 'table' }
+    { type: 'table' },
+    { type: 'newCompo' }
   ];
+
+
 
   componentMap: { [key: string]: any } = {
     chart: ChartWidgetComponent,
     report: ReportWidgetComponent,
-    table: TableWidgetComponent
+    table: TableWidgetComponent,
+    newCompo:NewComponentComponent
   };
+
+  drop(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.widgets, event.previousIndex, event.currentIndex);
+  }
+
+  removeWidget(index: number) {
+    this.widgets.splice(index, 1);
+  }
+
+  addWidget(type: string) {
+    //this.widgets.push({ type });
+
+    const widgetExists = this.widgets.some(widget => widget.type === type);
+
+    if (!widgetExists) {
+      this.widgets.push({ type });
+    } else {
+      alert(`${type} widget already added!`);
+    }
+
+
+  }
+
+
 
 }
